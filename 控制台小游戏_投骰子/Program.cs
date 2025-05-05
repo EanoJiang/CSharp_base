a32d96791f69c629efd8937d2de7d6816e0cbc97
Eano <934154737@qq.com>
2025-05-05 16:14:45 +0800

    控制台项目

---
+++ 控制台小游戏_投骰子/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 控制台小游戏_投骰子
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
